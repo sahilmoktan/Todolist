7e76ad11a48a3bfec8e88c59e1872ce0bfb8bd60
sahilmoktan <moktanshail9@gmail.com>
2023-09-03 23:22:54 +0545

    basic todolist layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client"
+// import React, { useState } from 'react'
 
 const page = () => {
+  // const [title, settitle] = useState("")
   return (
-    <div>Sahil Todolist app</div>
-  )
+<>
+<h1 className='text-white text-center text-4xl p-5 font-bold bg-orange-600'>Sahil Todo list</h1>
+
+<div className="justify-items-center">
+
+<form>
+<input type="text" className='text-xl border-black border-2 rounded-md m-5 p-1' placeholder='Your Task' />
+
+<input type="text" className='text-xl border-black border-2 rounded-md m-5 p-1' placeholder=' Task Discription' />
+
+<button className='bg-orange-600 font-bold text-white p-2 rounded-md'>Add Task</button>
+
+</form>
+</div>
+</>
+    )
 }
 
 export default page
