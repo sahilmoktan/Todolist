c695fb38c95b96107d98951600c25d4a74df18aa
sahilmoktan <moktanshail9@gmail.com>
2023-09-25 19:19:55 +0545

    showing task perfect

--- app/page.js
+++ app/page.js
@@ -4,16 +4,29 @@ import React, { useState } from "react";
 const page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  
+  const [mainTask, setMainTask] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log(title)
-    console.log(desc)
+    setMainTask([...mainTask, {title, desc}])
     setTitle("")
     setDesc("")
 
   };
+  let renderTask = <h2>No Task Available</h2>
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t,i)=>{
+      return (
+        <ul>
+        <div className="flex justify-between mb-5">
+          <h5 className="text-xl font-semibold">{t.title} </h5>
+          <h6 className="text-xl font-semibold">{t.desc} </h6>
+        </div>
+  
+        </ul>
+      )
+    })
+  }
 
   return (
     <>
@@ -47,6 +60,8 @@ const page = () => {
             Add Task
           </button>
         </form>
+        <hr/>
+        <div className="p-8 bg-slate-300">{renderTask}</div>
       </div>
     </>
   );
